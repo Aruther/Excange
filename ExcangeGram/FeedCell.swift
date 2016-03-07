1c9c996aa3dfd8ffb94a9c20232d9729c864704d
Parker Brown <parkerbrown@Parkers-MacBook-Pro.local>
2016-03-07 14:14:11 -0700

    first commit

---
+++ ExcangeGram/FeedCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCell.swift
+//  ExcangeGram
+//
+//  Created by Parker Brown on 3/3/16.
+//  Copyright © 2016 Parker Brown. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+}
